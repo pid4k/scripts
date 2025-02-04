467f6f10c85c19253a5bbcfbe50825df191b9f54
pid4k <165833162+pid4k@users.noreply.github.com>
2025-02-04 15:46:21 +0100

    Update BeanzUI.lua

--- BeanzUI.lua
+++ BeanzUI.lua
@@ -554,9 +554,43 @@ function BeanzUI:new(args)
 	local oldlinezindex = UI["adf"].ZIndex
 	local oldlogozindex = UI["Iconlol"].ZIndex
 
-	local olddrag = Instance.new("UIDragDetector")
-	olddrag.Parent = UI["2"]
+	local function dragGUI(gui)
+	task.spawn(function()
+		local dragging
+		local dragInput
+		local dragStart = Vector3.new(0,0,0)
+		local startPos
+		local function update(input)
+			local delta = input.Position - dragStart
+			local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+			TweenService:Create(gui, TweenInfo.new(.20), {Position = Position}):Play()
+		end
+		gui.InputBegan:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+				dragging = true
+				dragStart = input.Position
+				startPos = gui.Position
 
+				input.Changed:Connect(function()
+					if input.UserInputState == Enum.UserInputState.End then
+						dragging = false
+					end
+				end)
+			end
+		end)
+		gui.InputChanged:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+				dragInput = input
+			end
+		end)
+		UserInputService.InputChanged:Connect(function(input)
+			if input == dragInput and dragging then
+				update(input)
+			end
+		end)
+	end)
+	end
+	dragGUI(UI["1"])
 	UI["d"].MouseButton1Click:Connect(function()
 		if args.Movable then
 			if olddrag then
