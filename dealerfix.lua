bb866b15534be31b2e2424479edfb142b5152dcb
pid4k <165833162+pid4k@users.noreply.github.com>
2025-10-26 16:36:19 +0100

    Add dealer events to dealerfix.lua

---
+++ dealerfix.lua
@@ -0,0 +1,4 @@
+return {
+	dealersellevent = game:GetService("ReplicatedStorage"):WaitForChild("8WX"):WaitForChild("523be4a6-b3ed-4056-99dd-693827918ec9"),
+	dealerbuyevent = game:GetService("ReplicatedStorage"):WaitForChild("8WX"):WaitForChild("c945bfd5-0319-4bb7-8bfd-5ab218d3efe3")
+}
