2a008a526e097f900099bdac716abd9dd303ebbe
pid4k <165833162+pid4k@users.noreply.github.com>
2025-12-11 14:26:08 +0100

    Update dealersellevent ID in dealerfix.lua

--- dealerfix.lua
+++ dealerfix.lua
@@ -1,4 +1,4 @@
 return {
-	dealersellevent=game:GetService("ReplicatedStorage"):FindFirstChild("523be4a6-b3ed-4056-99dd-693827918ec9",true),
+	dealersellevent=game:GetService("ReplicatedStorage"):FindFirstChild("7c0c6f72-73f4-48a5-81e6-8ccf02a96366",true),
 	dealerbuyevent=game:GetService("ReplicatedStorage"):FindFirstChild("c82cec53-dd03-4db0-aa12-1fc14334ffe0",true)
 }
