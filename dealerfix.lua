b6b7df4bab73c15e5e00ba6b09a15d0511c26e3f
pid4k <165833162+pid4k@users.noreply.github.com>
2025-12-14 09:54:56 +0100

    Update dealerfix.lua

--- dealerfix.lua
+++ dealerfix.lua
@@ -1,4 +1,4 @@
 return {
-	dealersellevent=game:GetService("ReplicatedStorage"):FindFirstChild("7c0c6f72-73f4-48a5-81e6-8ccf02a96366",true),
-	dealerbuyevent=game:GetService("ReplicatedStorage"):FindFirstChild("c82cec53-dd03-4db0-aa12-1fc14334ffe0",true)
+	dealersellevent=game:GetService("ReplicatedStorage"):FindFirstChild("9efc661a-1220-4e52-ad80-284895b046ed",true),
+	dealerbuyevent=game:GetService("ReplicatedStorage"):FindFirstChild("c1432c43-51d9-490e-a001-9bf108c9750b",true)
 }
